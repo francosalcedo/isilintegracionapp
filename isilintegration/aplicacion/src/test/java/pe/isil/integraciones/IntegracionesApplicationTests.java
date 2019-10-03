96c4428d443214098fd30f9c64a7973f55127c0d
Franco Salcedo <franco.salcedo.i3@gmail.com>
2019-10-03 16:16:38 -0500

    init spring

---
+++ isilintegration/aplicacion/src/test/java/pe/isil/integraciones/IntegracionesApplicationTests.java
@@ -0,0 +1,16 @@
+package pe.isil.integraciones;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntegracionesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
